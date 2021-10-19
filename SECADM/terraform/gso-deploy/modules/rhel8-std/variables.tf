b2481df059050483d290db5133ea25bcf4eac952
John Best <jbest@redhat.com>
2021-10-19 14:13:58 -0500

    fix

--- SECADM/terraform/gso-deploy/modules/rhel8-std/variables.tf
+++ SECADM/terraform/gso-deploy/modules/rhel8-std/variables.tf
@@ -1,3 +1,19 @@
+variable "vsphere_server" {
+  description = "vSphere server"
+  type        = string
+}
+
+variable "vsphere_user" {
+  description = "vSphere username"
+  type        = string
+}
+
+variable "vsphere_password" {
+  description = "vSphere password"
+  type        = string
+  sensitive   = true
+}
+
 variable "datacenter" {
   description = "vSphere data center"
   type        = string
